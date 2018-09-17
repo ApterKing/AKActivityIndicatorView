12b16dfd6aab1ce7218a7ff9b4f9e39767b4af74
wangcong <wangcong@medlinker.com>
2018-09-17 15:07:57 +0800

    feat(6.8.2): 修改为重绘机制，更加像原生UIActivityIndicatorView靠近

--- AKActivityIndicatorView/AKActivityIndicatorView.swift
+++ AKActivityIndicatorView/AKActivityIndicatorView.swift
@@ -48,6 +48,11 @@ class AKActivityIndicatorView: UIView {
         _initUI()
     }
     
+    deinit {
+        _displayLink.invalidate()
+        _displayLink = nil
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
@@ -61,6 +66,7 @@ class AKActivityIndicatorView: UIView {
     private let _preIndicatorLayer = AKIndicatorLayer()
     private let _indicatorLayer = AKIndicatorLayer()
     private var _isAnimating = false
+    private var _displayLink: CADisplayLink!
 }
 
 extension AKActivityIndicatorView {
@@ -69,33 +75,41 @@ extension AKActivityIndicatorView {
         backgroundColor = UIColor.clear
         
         let outterRadius = _preIndicatorLayer.outterRadius
-        let lineWidth = _preIndicatorLayer.lineWidth
-        
         let originCenter = center
-        self.frame = CGRect(x: 0, y: 0, width: 2 * outterRadius + lineWidth, height: 2 * outterRadius + lineWidth)
+        self.frame = CGRect(x: 0, y: 0, width: 2 * outterRadius, height: 2 * outterRadius)
         self.center = originCenter
-
-        _preIndicatorLayer.frame = bounds //CGRect(x: 0, y: 0, width: 2 * outterRadius + lineWidth, height: 2 * outterRadius + lineWidth)
-        layer.addSublayer(_preIndicatorLayer)
-        _preIndicatorLayer.setNeedsDisplay()
-        
-        _indicatorLayer.isHidden = true
-        _indicatorLayer.frame = _preIndicatorLayer.frame
-        _indicatorLayer.alphaGradient = 0.05
-        layer.insertSublayer(_indicatorLayer, below: _preIndicatorLayer)
-        _indicatorLayer.setNeedsDisplay()
         
         let maskLayer = CAShapeLayer()
-        maskLayer.path = UIBezierPath(arcCenter: CGPoint(x: outterRadius + lineWidth / 2.0, y: outterRadius + lineWidth / 2.0), radius: outterRadius + lineWidth / 2.0, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2 * 3, clockwise: true).cgPath
-        maskLayer.lineWidth = outterRadius * 2 + lineWidth
+        maskLayer.path = UIBezierPath(arcCenter: CGPoint(x: outterRadius, y: outterRadius), radius: outterRadius, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2 * 3, clockwise: true).cgPath
+        maskLayer.lineWidth = outterRadius * 2
         maskLayer.strokeColor = UIColor.red.cgColor
         maskLayer.fillColor = UIColor.clear.cgColor
         maskLayer.strokeEnd = 0.75
-        layer.addSublayer(maskLayer)
         layer.mask = maskLayer
         strokeEnd = 0.0
+
+        _preIndicatorLayer.frame = bounds
+        layer.addSublayer(_preIndicatorLayer)
+        _preIndicatorLayer.setNeedsDisplay()
+        
+        _indicatorLayer.frame = _preIndicatorLayer.frame
+        _indicatorLayer.color = color.withAlphaComponent(0.5)
+        _indicatorLayer.highlightColor = color
+        _indicatorLayer.highlightRange = 10..<12
+        layer.insertSublayer(_indicatorLayer, below: _preIndicatorLayer)
+        _indicatorLayer.setNeedsDisplay()
+        
+        _displayLink = CADisplayLink(target: self, selector: #selector(_redrawAction))
+        _displayLink.add(to: RunLoop.current, forMode: .defaultRunLoopMode)
+        _displayLink.preferredFramesPerSecond = 16
+        _displayLink.isPaused = true
     }
     
+    @objc func _redrawAction() {
+        let range = _indicatorLayer.highlightRange
+        _indicatorLayer.highlightRange = (range.lowerBound + 1)..<(range.upperBound + 1)
+        _indicatorLayer.setNeedsDisplay()
+    }
 }
 
 extension AKActivityIndicatorView {
@@ -104,18 +118,17 @@ extension AKActivityIndicatorView {
         _isAnimating = true
         _preIndicatorLayer.isHidden = true
         _indicatorLayer.isHidden = false
-
-        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
-        animation.duration = 0.75
-        animation.toValue = Float.pi * 2
-        animation.repeatCount = MAXFLOAT
-        _indicatorLayer.add(animation, forKey: "rotation")
+        
+        _displayLink.isPaused = false
     }
     
     func stopAnimation() {
-        _indicatorLayer.removeAllAnimations()
+        _displayLink.isPaused = true
+
         _preIndicatorLayer.isHidden = false
         _indicatorLayer.isHidden = true
+        _indicatorLayer.highlightRange = 10..<12
+        _indicatorLayer.setNeedsDisplay()
         _isAnimating = false
     }
 }
@@ -124,24 +137,32 @@ extension AKActivityIndicatorView {
 
     class AKIndicatorLayer: CALayer {
         
-        fileprivate let outterRadius: CGFloat = 15
-        fileprivate let innerRadius: CGFloat = 8
+        fileprivate let outterRadius: CGFloat = 14
+        fileprivate let innerRadius: CGFloat = 7
         fileprivate let lineWidth: CGFloat = 2.5
+
+        var highlightRange = 0..<0 {
+            didSet{
+                displayIfNeeded()
+            }
+        }
+        var highlightColor = UIColor.white {
+            didSet {
+                displayIfNeeded()
+            }
+        }
         
-        /// 线条颜色
         var color: UIColor = UIColor.white {
             didSet {
-                self.displayIfNeeded()
+                displayIfNeeded()
             }
         }
-        
-        /// 线条梯度
         var alphaGradient: CGFloat = 0.0 {
             didSet {
-                self.displayIfNeeded()
+                displayIfNeeded()
             }
         }
-        
+
         override func draw(in ctx: CGContext) {
             ctx.setFillColor(UIColor.clear.cgColor)
             ctx.setLineWidth(lineWidth)
@@ -149,13 +170,32 @@ extension AKActivityIndicatorView {
             for i in 0..<12 {
                 let x = cos(CGFloat.pi / 2.0 - CGFloat(i) * 30.0 * CGFloat.pi / 180.0)
                 let y = sin(CGFloat.pi / 2.0 - CGFloat(i) * 30.0 * CGFloat.pi / 180.0)
-                let from = CGPoint(x: outterRadius + x * outterRadius + lineWidth / 2.0, y: outterRadius - y * outterRadius + lineWidth / 2.0)
-                let to = CGPoint(x: outterRadius + x * innerRadius + lineWidth / 2.0, y: outterRadius - y * innerRadius + lineWidth / 2.0)
+                
+                let realOutterRadius = outterRadius - lineWidth / 2.0
+                let from = CGPoint(x: realOutterRadius + x * realOutterRadius + lineWidth / 2.0, y: realOutterRadius - y * realOutterRadius + lineWidth / 2.0)
+                let to = CGPoint(x: realOutterRadius + x * innerRadius + lineWidth / 2.0, y: realOutterRadius - y * innerRadius + lineWidth / 2.0)
+                ctx.setAllowsAntialiasing(true)
                 ctx.saveGState()
-                ctx.setStrokeColor(color.withAlphaComponent(0.8 -  (12.0 - CGFloat(i)) * alphaGradient).cgColor)
+                var alphaColor = color
+                if alphaGradient == 0.0 {
+                    alphaColor = color
+                } else {
+                    var alpha = 1 -  (12.0 - CGFloat(i)) * alphaGradient
+                    alpha = alpha < 0.5 ? 0.5 : alpha
+                    alphaColor = color.withAlphaComponent(alpha)
+                }
+                for highlight in highlightRange {
+                    if highlight % 12 == i {
+                        alphaColor = highlightColor
+                        break
+                    }
+                }
+                ctx.setStrokeColor(alphaColor.cgColor)
                 ctx.move(to: from)
                 ctx.addLine(to: to)
+                ctx.drawPath(using: .fillStroke)
                 ctx.strokePath()
+                ctx.closePath()
                 ctx.restoreGState()
             }
         }
